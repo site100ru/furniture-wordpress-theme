5838179fff2ebe4c5a3b52c6e2ca893b326996c4
Slava_web_student <84717237+SlavaWebDeveloper@users.noreply.github.com>
2026-01-12 18:58:54 +0700

    3.1.4
    Update titles within product card

--- woocommerce/content-kitchen.php
+++ woocommerce/content-kitchen.php
@@ -54,12 +54,12 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	
 		<div class="card-wrapper project-container-2-footer">
 			<div class="row" style="padding: 10px;">
-				<div class="col-10">
+				<div class="col-12">
 					<h3 style="color: #fff;">
 						<?php do_action( 'woocommerce_shop_loop_item_title' ); ?>
 					</h3>		
 				</div>
-				<div class="col-10 pc-sell">
+				<div class="col-12 pc-sell">
 					<h3 2 style="color: #fff;" class="text-start-product text-md-end-product">
 						<?php do_action( 'woocommerce_after_shop_loop_item_title' ); ?>
 					</h3>
